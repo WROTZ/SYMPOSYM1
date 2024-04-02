c23f1e276a908277108b2d85a4ab90c0ea3af81d
WROTZ <94730219+WROTZ@users.noreply.github.com>
2024-04-02 09:40:46 +0530

    Add files via upload

---
+++ 1)3 numbers edited.py
@@ -0,0 +1,12 @@
+a=int(Input("Enter first number:"))
+b=int(Input("Enter second number:"))
+c=int(Input("Enter third number:"))
+d=[]
+d.append(a);
+d.append(b);
+d.append(c);
+for i in range(0,3)
+    for j in range(0,3)
+        for k in range(0,3)
+            if(i!=j,j!=k&,k!=i);
+                print(d[i].d[j],d[k])
